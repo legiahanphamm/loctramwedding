3582864bdc6a47eed14c84ebea53c697fce19a16
Pham Le Gia Han <giahanphamle.se@gmail.com>
2026-04-08 02:50:30 +0700

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import "./globals.css";
 import { Playfair_Display } from "next/font/google";
+import type { Metadata } from "next";
 
 const font = Playfair_Display({
   subsets: ["latin"],
   weight: ["400", "600"],
 });
+
+export const metadata: Metadata = {
+  title: "L&T Wedding",
+};
 
 export default function RootLayout({
   children,
